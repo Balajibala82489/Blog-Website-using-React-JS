45d4355d8a7b76d3694fa38914cd2ac3b915df8a
Balaji k <balajikph@gmail.com>
2024-09-02 21:02:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base="/Blog-Website-React-JS/"
   server: {
     port: 3000,
   },
